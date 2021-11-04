f61ad09e3f43b79b886c900512726401982c766a
Amjed Ali K <amjedmgm@gmail.com>
2021-11-04 21:15:52 +0530

    :bug:  Fixing Permissions

--- constants/permissions.py
+++ constants/permissions.py
@@ -2,14 +2,14 @@ from starlette import status
 from fastapi import HTTPException
 from models.user import AccountType
 
-admin_access_permission = ['admin', 'staff']
+admin_access_permission = ['admin']
 
 post_permissions = {
-    'post_view': [AccountType.staff],
-    'post_edit': [AccountType.staff],
-    'post_respond': [AccountType.staff, AccountType.parent],
-    'post_delete': [AccountType.staff],
-    'response_delete': [AccountType.staff]
+    'post_view': [AccountType.grievance_cell_memeber],
+    'post_edit': [AccountType.grievance_cell_memeber],
+    'post_respond': [AccountType.grievance_cell_memeber, AccountType.parent],
+    'post_delete': [AccountType.grievance_cell_memeber],
+    'response_delete': [AccountType.grievance_cell_memeber]
 }
 
 no_permission = HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="You don't have permissions for this action!")
