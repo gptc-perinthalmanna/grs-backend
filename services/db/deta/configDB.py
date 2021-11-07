07773294f33c9a8ffdd012938d580bcdfa701169
Amjed Ali K <amjedmgm@gmail.com>
2021-11-07 17:53:06 +0530

    :bug: Fixed Config DB fuctions

--- services/db/deta/configDB.py
+++ services/db/deta/configDB.py
@@ -10,7 +10,7 @@ config_db = deta.Base('config')
 def get_config(config_name):
     _config = config_db.get(config_name)
     if _config is None:
-        put_config({}, config_name)
+        put_config(config_name, {})
         return {}
     return _config
 
@@ -20,7 +20,7 @@ def put_config(config_name, config_value):
 def get_chat_ids():
     bot_config = get_config("telegram")
     if bot_config is None:
-        put_config({"telegram_connected_chats": []}, "telegram")
+        put_config("telegram", {"telegram_connected_chats": []})
         return []
     return bot_config["telegram_connected_chats"]
 
@@ -28,4 +28,4 @@ def put_chat_id(chat_id):
     new_config =  get_config("telegram")
     if new_config["telegram_connected_chats"] is None: new_config["telegram_connected_chats"] = []
     new_config["telegram_connected_chats"].append(chat_id)
-    put_config(new_config, "telegram")
+    put_config("telegram", new_config)
